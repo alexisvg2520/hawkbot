8667bed343302918732f9ed4203c6c023461c79e
GusAdolf <49161487+GusAdolf@users.noreply.github.com>
2021-12-08 11:28:48 -0500

    creacion de bdd y tablas

--- dist/php/conexion.php
+++ dist/php/conexion.php
@@ -31,26 +31,48 @@ try {
     $sql1 = "CREATE TABLE ventas_uio (
     Fecha DATE,
     Nombre_producto VARCHAR(40) NOT NULL,
-    Costos INT(10) NOT NULL,
+    Costos INT(10) NOT NULL
     )";
 
-    $sql2 = "CREATE TABLE ventas_gye(
-    Fecha DATE,
-    Nombre_producto VARCHAR(40) NOT NULL,
-    Costos INT(10) NOT NULL,
-    )";
-    
-  
+     
     // use exec() because no results are returned
     $conn1->exec($sql1);
-    $conn1->exec($sql2);
+     
     echo "Tabla ventas_uio creada correctamente";
-    echo "Tabla ventas_gye creada correctamente";
+    
   } catch(PDOException $e) {
     echo $sql1 . "<br>" . $e->getMessage();
-    echo $sql2 . "<br>" . $e->getMessage();
+  
   }
   
   $conn1 = null;
+
+  $conn2 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  try {
+      $conn2 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+      // sql to create table
+    $sql2 = "CREATE TABLE ventas_gye(
+      Fecha DATE,
+      Nombre_producto VARCHAR(40) NOT NULL,
+      Costos INT(10) NOT NULL
+      )";
+      
+    
+      // use exec() because no results are returned
+     
+      $conn2->exec($sql2);
+     
+    echo "Tabla ventas_gye creada correctamente";
+    } catch(PDOException $e) {
+      
+      echo $sql2 . "<br>" . $e->getMessage();
+    
+    }
+    
+    $conn2 = null;
+
 
 ?>
