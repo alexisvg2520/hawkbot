5fe036316380ac7e6fb7277c40a86688f48492ef
alexisvg2520 <78839355+alexisvg2520@users.noreply.github.com>
2021-12-08 22:44:53 -0500

    cambio2_avg

--- dist/php/db.inc2.php
+++ dist/php/db.inc2.php
@@ -4,8 +4,9 @@ $username = "prueba";
 $password = "c8u$7ShG";
 $dbname = "reportes";
 
-$connec = mysqli_connect($servername,$username,$password);
+$connec = mysqli_connect($servername,$username,$password, $dbname);
 
-mysqli_select_db($dbname,$connec);
-
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+  }
 ?>
